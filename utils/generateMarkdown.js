35732f7b76ba7692353e6a87d1afc57ec395a3c0
cnl-95 <98615179+cnl-95@users.noreply.github.com>
2022-07-18 01:23:35 -0400

    return a license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // function to render the badge based on the selected license to post to the README
-
+function renderLicenseBadge(license) {
     // switch statement to render the badge based on the selected license
-
+    switch (license) {
       // name of the license
-    
+      case 'MIT':
         // returns the markdown badge for the license with the link to the license
-      
+        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      case 'Apache':
+        return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      case 'GPL':
+        return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      case 'BSD':
+        return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+      default:
         // If there is no license, return an empty string
-   
+        return '';
+    }
+  }
   // TODO: Create a function that returns the license link
   //  function to render the license link based on the selected license to post to the README
 
