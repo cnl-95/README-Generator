aed6f08d62cec720880d1121edbae4928950625d
cnl-95 <98615179+cnl-95@users.noreply.github.com>
2022-07-18 01:24:48 -0400

    finished MD todos

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -40,28 +40,77 @@ function renderLicenseLink(license) {
   }
   
   // TODO: Create a function that returns the license section of README
-  //  function to render the license section and link based on the selected license to post to the README
- 
+//  function to render the license section and link based on the selected license to post to the README
+function renderLicenseSection(license) {
     // switch statement to render the badge and link based on the selected license
- 
+    const licenseSection = `## License`
+    switch (license) {
       // name of the license
-    
+    case 'MIT':
       // returns the  markdown section license and the link to the license
-  
+    return `${licenseSection}
+    This project is licensed under the MIT license.
+    To learn more about the MIT license, visit their site at ${renderLicenseLink(license)}`;
+    case 'Apache':
+     return `${licenseSection}
+     This project is licensed under the Apache license.
+    To learn more about the Apache license, visit their site at ${renderLicenseLink(license)}`;
+    case 'GPL':
+    return `${licenseSection}
+    This project is licensed under the GPL license.
+    To learn more about the GPL license, visit their site at ${renderLicenseLink(license)}`;
+    case 'BSD':
+    return `${licenseSection}
+    This project is licensed under the BSD license.
+    To learn more about the BSD license, visit their site at ${renderLicenseLink(license)}`;
+    default:
       // If there is no license, return an empty string
-
+    return '';
+    };
+  };
   
   // function to generate link to the user's GitHub profile
-
+  function generateGitHubUserName(username) {
     // returns the markdown link to the user's GitHub profile
+    return `[${username}](https://github.com/${username})`;
+  }
   
   // TODO: Create a function to generate markdown for README
-  //  function to generate the markdown for the README
-  //  ${data.title} and others are the values that are passed in from the user in the promptUser function
-  /*  $renderLicenseBadge(data.license) and $renderLicenseSection(data.license) are the 
-  values that are passed in from the renderLicenseBadge and renderLicenseSection functions */
-  
-  
+//  function to generate the markdown for the README
+//  ${data.title} and others are the values that are passed in from the user in the promptUser function
+/*  $renderLicenseBadge(data.license) and $renderLicenseSection(data.license) are the 
+values that are passed in from the renderLicenseBadge and renderLicenseSection functions */
+
+function generateMarkdown(data) {
+    return `
+    # ${data.title}
+  ${renderLicenseBadge(data.license)}
+    ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    
+    ## Description 
+  ${data.description}
+    ## Installation
+  ${data.installation}
+    ## Usage
+  ${data.usage}
+    ${renderLicenseSection(data.license)}
+    ## Contributing
+  ${data.contributing}
+    ## Tests
+  ${data.tests}
+    ## Questions
+  Thank you so much for using my application! 
+  Feel free to reach out to me at ${data.email} 
+  or visit my GitHub profile at ${generateGitHubUserName(data.username)}
+  `;
+  }
   
   //  export the generateMarkdown function so it can be used in other files
- 
+  module.exports = generateMarkdown;
