5f2df37fee9784e68144f6f5b63e462c50578b61
cnl-95 <98615179+cnl-95@users.noreply.github.com>
2022-07-18 01:22:57 -0400

    added todo comments for md gen

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,48 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// function to render the badge based on the selected license to post to the README
+
+    // switch statement to render the badge based on the selected license
+
+      // name of the license
+    
+        // returns the markdown badge for the license with the link to the license
+      
+        // If there is no license, return an empty string
+   
+  // TODO: Create a function that returns the license link
+  //  function to render the license link based on the selected license to post to the README
+
+    // switch statement to render the link based on the selected license
+
+      // name of the license
+   
+        // returns the link to the license
+     
+        // If there is no license, return an empty string
+  
+  // TODO: Create a function that returns the license section of README
+  //  function to render the license section and link based on the selected license to post to the README
+ 
+    // switch statement to render the badge and link based on the selected license
+ 
+      // name of the license
+    
+      // returns the  markdown section license and the link to the license
+  
+      // If there is no license, return an empty string
+
+  
+  // function to generate link to the user's GitHub profile
+
+    // returns the markdown link to the user's GitHub profile
+  
+  // TODO: Create a function to generate markdown for README
+  //  function to generate the markdown for the README
+  //  ${data.title} and others are the values that are passed in from the user in the promptUser function
+  /*  $renderLicenseBadge(data.license) and $renderLicenseSection(data.license) are the 
+  values that are passed in from the renderLicenseBadge and renderLicenseSection functions */
+  
+  
+  
+  //  export the generateMarkdown function so it can be used in other files
+ 
